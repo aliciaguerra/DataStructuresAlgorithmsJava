01e14d158b81762dc92146bf638250ff3c06a7b3
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-26 22:30:52 -0700

    Create switchdays.java

---
+++ src/datastructuresalgorithmsjava/switchdays.java
@@ -0,0 +1,20 @@
+switch(d){
+case MON;
+System.out.println("This is tough.");
+break;
+case TUE:
+System.out.println("This is getting better.");
+break;
+case WED:
+System.out.println("Half way there.");
+break;
+case THU:
+System.out.println("I can see the light.");
+break;
+case FRI:
+System.out.println("Now we are talking.");
+break;
+default:
+System.out.println("Day off!");
+break;
+}
