7214b85caf8758614e1a0ae215e7a0fa7dcab34f
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-26 22:20:59 -0700

    Create DayTripper.java

---
+++ src/datastructuresalgorithmsjava/DayTripper.java
@@ -0,0 +1,9 @@
+public class DayTripper{
+public enum Day {MON, TUE, WED, THU, FRI, SAT, SUN};
+public static void main(Strings[] args){
+Day d=Day MON;
+System.out.println("Initially d is" +d);
+Day t=Day.valueOf("WED");
+System.out.println("I say d and t are the same: " +(d==t));
+}
+}
